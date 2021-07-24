f8f9d9b6d39c6efa20fcad44cfc5ffa79919cd9d
Jack Etheredge <jack.etheredge@gmail.com>
2021-07-24 08:06:10 -0400

    create todo, extract more code distinguishing duplicated notebooks

---
+++ TODO.md
@@ -0,0 +1,6 @@
+## TODO:
+- Click CLI controlling preprocessing and training behavior
+- Unit tests for CLI and Flask app
+- Logging
+- Poetry or pipenv
+- Download the dataset if it's not already present in /data
--- ml/deduplicating_patients.py
+++ ml/deduplicating_patients.py
@@ -1,7 +1,16 @@
-# Removing expired patients:
-patientdata = patientdata[patientdata.discharge_disposition_id.str.contains("Expired") == False]
-print(patientdata.shape)
+"""Extracting the differences between the duplicated notebooks"""
 
-# Removing repeat patient entries (since they violate independence):
-patientdata = patientdata.groupby('patient_nbr', group_keys=False).apply(lambda x: x.loc[x.encounter_id.idxmin()])
-print(patientdata.shape)
+if args.get(remove_expired):
+    # Removing expired patients:
+    patientdata = patientdata[patientdata.discharge_disposition_id.str.contains("Expired") == False]
+    print(patientdata.shape)
+
+if args.get(remove_duplicates):
+    # Removing repeat patient entries (since they violate independence):
+    patientdata = patientdata.groupby('patient_nbr', group_keys=False).apply(lambda x: x.loc[x.encounter_id.idxmin()])
+    print(patientdata.shape)
+
+if args.get(binary_classification):
+    y = y.str.replace('>30','NO')
+    y_test = y_test.str.replace('>30','NO')
+    y_train = y_train.str.replace('>30','NO')
